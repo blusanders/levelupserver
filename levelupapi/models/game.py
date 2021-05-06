3e77f79867b4caa895c487c0dfc4583dda51ee11
blusanders <blusanders@yahoo.com>
2021-05-06 09:21:02 -0500

    Moving to serializers

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -3,6 +3,7 @@ from levelupapi.models.game_type import GameType
 
 class Game(models.Model):
     title = models.CharField(max_length=55)
+    maker = models.CharField(max_length=55)
     game_type = models.ForeignKey(GameType, on_delete=models.CASCADE,)
     number_of_players = models.IntegerField(default=1)
     skill_level = models.IntegerField(default=1)
